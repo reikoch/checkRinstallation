ac2239372bb8bda74b783789d33895e0e8df643f
Reinhold <8436725+reikoch@users.noreply.github.com>
2019-05-10 17:29:52 +0200

    get download tarballs working

--- R/gettars.R
+++ R/gettars.R
@@ -3,5 +3,7 @@
 
 library(remotes)
 
-inst <- installed.packages()[, c('Package','Version')]
-tarbs <- apply(inst, 1, remotes::download_version, type='source')
+i <- installed.packages()
+inst <- i[is.na(i[,'Priority']) | i[,'Priority'] != 'base', c('Package','Version')]
+rm(i)
+tarbs <- apply(inst, 1, function(X) try(remotes::download_version(X[1], version = X[2], type='source')))
