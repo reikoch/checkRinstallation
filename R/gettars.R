ef017295d72250f6731239f6b14014d0ce273e9c
Reinhold <8436725+reikoch@users.noreply.github.com>
2019-05-11 15:16:56 +0200

    works for CRAN packages

--- R/gettars.R
+++ R/gettars.R
@@ -6,4 +6,18 @@ library(remotes)
 i <- installed.packages()
 inst <- i[is.na(i[,'Priority']) | i[,'Priority'] != 'base', c('Package','Version')]
 rm(i)
-tarbs <- apply(inst, 1, function(X) try(remotes::download_version(X[1], version = X[2], type='source')))
+avail <- available_packages()[, c('Package', 'Repository')]
+pkgsrc <- merge(inst, avail)
+
+# retrieve tarball from repo, in second attempt from repo/Archive
+getsrc <- function(pkg, version, repo, destdir) {
+  fi <- paste0(repo, '/', pkg, "_", version, '.tar.gz')
+  if (!(attr(curlGetHeaders(fi), 'status', exact = TRUE) %in% c(200, 0))) {
+    fi <- paste0(repo, '/Archive/', pkg, '/', pkg, "_", version, '.tar.gz')
+  }
+  curl::curl_download(fi, file.path(destdir, basename(fi)))
+}
+
+# create donwload directory and do it
+dir.create(dwnld <- tempfile('pkgs_'))
+tarbs <- apply(pkgsrc, 1, function(X) getsrc(X[1], X[2], X[3], destdir=dwnld))
